eaaa4ed63e36d57f8223801ee7b0e4ef749d67cb
Alice Villar <alsaldanhavillar@gmail.com>
2021-07-20 10:09:32 -0300

    update

--- stylelint.py
+++ stylelint.py
@@ -1,34 +1,40 @@
 # CODE SOURCE: SOFTWARE ARCHITECTURE WITH PYTHON
 
+###################################################################################
+#
+# Question 1:
+#
+# def factorial(n):
+# if n == 0:
+# return 1
+# else:
+# return n*factorial(n-1)
+#
+####################################################################################
 
 
-# Questão 1:
+# It's necessary to fix identation:
 
-def factorial(n):
-if n == 0:
-return 1
-else:
-return n*factorial(n-1)
-
-
-''' 
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n*factorial(n-1)
 
-'''
+n=int(input("Input a number to see the factiorial : ")) #receive input from user
+print(factorial(n)) # printing the result
 
-# Questão 2:
+####################################################################################
+# Another version:factorial function is defined only for positive integers
+####################################################################################
+# The factorial function where input is a non-negative integer
 
-# pylint -E stylelint.py = comando p/ buscar errors
-# Output => stylelint.py:9:1: E0001: invalid syntax (<unknown>, line 9) (syntax-error)
+def factorial(n):
+    if n < 0 or type(n) is float:
+        return("Number cannot be negative or floating point!")
+    return 1 if n == 0 else n * factorial(n - 1)
 
-# pylint -rn stylelint.py = >
+n=abs(int(input("Input a number to see the factiorial : ")))
+print(factorial(n))
 
-# stylelint.py:9:1: E0001: invalid syntax (<unknown>, line 9) (syntax-error)
-
-
-# OUTPUT => SyntaxError: Missing parentheses in call to 'print'. Did you mean print(encoded)?
 
