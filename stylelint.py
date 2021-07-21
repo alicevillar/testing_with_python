b81e2bb4386419cb061aee521ffd04c7dde987fe
Alice Villar <alsaldanhavillar@gmail.com>
2021-07-21 15:55:57 -0300

    including original stylelint.py and my solution

--- stylelint.py
+++
@@ -1,8 +0,0 @@
-# CODE SOURCE: SOFTWARE ARCHITECTURE WITH PYTHON
-
-def factorial(n):
-""" Return factorial of n """
-if n == 0:
-return 1
-else:
-return n*factorial(n-1)
--- stylelint_done.py
+++ stylelint_done.py
@@ -1,16 +1,39 @@
 # CODE SOURCE: SOFTWARE ARCHITECTURE WITH PYTHON
 
-def factorial(n):
-""" Return factorial of n """
-if n == 0:
-return 1
-else:
-return n*factorial(n-1)
-# CODE SOURCE: SOFTWARE ARCHITECTURE WITH PYTHON
+###################################################################################
+#
+# Original code to be fixed:
+#
+# def factorial(n):
+# if n == 0:
+# return 1
+# else:
+# return n*factorial(n-1)
+####################################################################################
+
+
+# It's necessary to fix identation:
 
 def factorial(n):
-""" Return factorial of n """
-if n == 0:
-return 1
-else:
-return n*factorial(n-1)
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+n=int(input("Input a number to see the factiorial : ")) #receive input from user
+print(factorial(n)) # printing the result
+
+####################################################################################
+# Another version:factorial function is defined only for positive integers
+####################################################################################
+# The factorial function where input is a non-negative integer
+
+def factorial(n):
+    if n < 0 or type(n) is float:
+        return("Number cannot be negative or floating point!")
+    return 1 if n == 0 else n * factorial(n - 1)
+
+n=abs(int(input("Input a number to see the factorial : ")))
+print(factorial(n))
+
+
