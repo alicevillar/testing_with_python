12f136c35f83a7bbdfce18dc7326ccbb92da36bc
Alice Villar <alsaldanhavillar@gmail.com>
2021-07-24 15:27:46 -0300

    update

--- metric_test_done.py
+++ metric_test_done.py
@@ -64,24 +64,3 @@ class DominoClass(CobraClass):
         return self.num_2 - self.num_1
 
 # Your code has been rated at 10.00/10 :)
-
-
-# parentesis, indentation, importing, deleted "Chapter 2 ",
-# changed the order expected_time & start_time
-# deleted import random because was not being used
-# deleted unecessary "else" in the last function
-# name of the last function has to be a string
-# In the last function, argument name "y" doesn't conform to snake_case naming style (invalid-name)
-# functions names must be pascal case
-# explicar o que cada funcao faz
-# inserir comentário dentro da funcao (qdo a função nao for um getter nem setter nem construtor)
-# else desnecessário na função def f_function
-# Unused argument 'x' (unused-argument) = criar variaveis para colovar o x e o y
-# nome da primeira funçao está mto grande. FOi preciso reduzir.
-# o nome do arquivo tem q respeitar o snake case
-# retirar o module "object" da primeira função porque é redundante
-# nome das funções tem que ser uma variável no estilo snakecase
-# improve  (chained-comparison)
-# Class name "D_class" e C doesn't conform to PascalCase naming style (invalid-name)
-# sobrecarga de operador - evitar - nao pode ter função com o mesmo nome em classes diferentes
-# Final newline missing (missing-final-newline)
--- stylelint_done.py
+++ stylelint_done.py
@@ -20,20 +20,8 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 
-n=int(input("Input a number to see the factiorial : ")) #receive input from user
+n=int(input("Input a number to see the factorial : ")) #receive input from user
 print(factorial(n)) # printing the result
 
-####################################################################################
-# Another version:factorial function is defined only for positive integers
-####################################################################################
-# The factorial function where input is a non-negative integer
-
-def factorial(n):
-    if n < 0 or type(n) is float:
-        return("Number cannot be negative or floating point!")
-    return 1 if n == 0 else n * factorial(n - 1)
-
-n=abs(int(input("Input a number to see the factorial : ")))
-print(factorial(n))
 
 
