0880da0f6e7419f8b6a77d77229a1e9fce60e1f0
Alice Villar <alsaldanhavillar@gmail.com>
2021-07-24 18:56:43 -0300

    update

--- sums.py
+++ sums.py
@@ -1,5 +1,6 @@
 # SOURCE OF CODE: https://realpython.com/python-testing/
 
+
 def test_sum():
     assert sum([1, 2, 3]) == 6, "Should be 6"
 
@@ -7,4 +8,8 @@ if __name__ == "__main__":
     test_sum()
     print("Everything passed")
 
-# https://pypi.org/project/pytest-mccabe/
+#1 pip install pytest-mccabe
+#2 pytest -q --mccabe module.py
+#3 ==>> 2 passed in 0.32s
+
+# python -m mccabe --min 5 mccabe.py
